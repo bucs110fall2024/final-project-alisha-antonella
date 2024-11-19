2457e85548879c5d47f646d84f97c52b9ab68d73
AntonellaGutierrez <agutierrezpi@binghamton.edu>
2024-11-19 15:06:51 -0500

    update 2

--- Final Project/template_final_project-master/src/costumer.py
+++ Final Project/template_final_project-master/src/costumer.py
@@ -1,3 +1,32 @@
 import pygame
 
 class Costumer:
+    
+    def __init__(self, x, y, img_file):
+         """
+         Initializes the costumer object
+         args:
+        - x (int) - starting x coordinate
+        - y (int) - starting y coordinate
+        - img_file : str - path to ____ file
+         """
+    
+    def move_to_order(self):
+        """
+        Moves the costumer object to the odering station
+    
+        """
+    
+    def order(self):
+        """
+        The costumer orders based off a randomly selected
+        order from the menu list
+    
+        """
+    
+    def review(self):
+        """
+        The costumer gives a review and gives a certain amount 
+        of tip based off of how well the player makes their order
+    
+        """
