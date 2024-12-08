b8353302cefd0897d2f899ca6b3a29292d914b60
AntonellaGutierrez <agutierrezpi@binghamton.edu>
2024-12-07 19:11:36 -0500

    original code backup

---
+++ Final Project/original code backup/budgeting2.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Budgeting:
+    def __init__(self, x, y):
+         """
+         Initializes the budgeting object/item on screen
+         Args: x (int) - starting x coordinate
+         y (int) - starting y coordinate
+         """
+         self.x = x
+        
+         self.y = y
+        
+         self.money = 0 
+        
+         self.font = pygame.font.SysFont("Arial", 24) 
+        
+         self.color = (0, 255, 0) 
+         
+         
+    def countMoney(self, amount):
+        """
+        Calculates and keeps track of the money earned by user
+        """
+        
+        self.money += amount
+         
+    def displaysMoney(self, screen):
+        """
+        Displays the current budget which the user 
+        has, in order to buy supplies etc. 
+        """
+        money_text = f"Money: ${self.money}"
+        
+        text_surface = self.font.render(money_text, True, self.color)
+        
+        screen.blit(text_surface, (self.x, self.y))  
+    
+    
+    def budgetPrediction(self, average_earnings, time_played):
+        """
+        Incorporate a feature in which based on the 
+        users usual earnings it can predict how much
+        user will have after a certain amount of time 
+        played.  
+        """
+        predicted_money = self.money + (average_earnings * time_played)
+        
+        return predicted_money
---
+++ Final Project/original code backup/music2.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Music:
+    
+    def __init__(self, music_file):
+        """
+        Initializes the Music class and starts playing the background music.
+        
+        args:
+        - music_file (str): path to the music file (e.g., "background_music.mp3")
+        """
+        pygame.mixer.init()
+        
+        self.music_file = music_file 
+         
+        self.play_music()  
+
+    def play_music(self):
+        """
+        Plays the background music in a loop.
+        """
+        try:
+            pygame.mixer.music.load(self.music_file)  
+            
+            pygame.mixer.music.play(-1) 
+            
+        except pygame.error as e:
+            
+            print(f"Error loading music file: {e}")
+
+    def stop_music(self):
+        """
+        Stops the background music.
+        """
+        pygame.mixer.music.stop()
+        
+        print("Music stopped.")
